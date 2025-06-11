cb098e62be08b5009464a58846a78a18c6d90631
André Iske <andre.iske@mailbox.org>
2025-06-12 00:11:08 +0200

    Some debugging

--- src/test/java/de/iske/kistogramm/config/DefaultCategoryInitializerTest.java
+++ src/test/java/de/iske/kistogramm/config/DefaultCategoryInitializerTest.java
@@ -52,6 +52,9 @@ class DefaultCategoryInitializerTest {
                 .distinct()
                 .count();
 
+        // print out the names of all categories for debugging
+        allCategories.forEach(category -> System.out.println("Category: " + category.getName()));
+
         assertThat(uniqueCategoryCount).isEqualTo(allCategories.size());
     }
 }
