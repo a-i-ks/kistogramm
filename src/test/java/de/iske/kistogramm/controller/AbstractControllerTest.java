209eed129670df98ee19c5b744fdc65e87f8e856
André Iske <andre.iske@mailbox.org>
2025-06-13 13:19:41 +0200

    Fix test

--- src/test/java/de/iske/kistogramm/controller/AbstractControllerTest.java
+++ src/test/java/de/iske/kistogramm/controller/AbstractControllerTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(properties = "spring.profiles.active=test")
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public abstract class AbstractControllerTest {
 
     @Autowired
